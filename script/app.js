a896b1ab156abbc147f09e313f41936b744e4126
Asqarbek Olimov <88034288+AsqarbekOlimov@users.noreply.github.com>
2022-06-15 11:01:19 +0500

    Home and about part finish

---
+++ script/app.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  var typed = new Typed('.animate', {
+    strings: [
+      'Frontend Developer',
+      'UI Designer',
+      'MERN Stack Developer',
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+  })
+});
