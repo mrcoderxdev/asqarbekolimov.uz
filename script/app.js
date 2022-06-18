72a00c0e0786c9bfd59d43495252c1331f70efff
Asqarbek Olimov <88034288+AsqarbekOlimov@users.noreply.github.com>
2022-06-18 12:47:20 +0500

    Sections part finish

--- script/app.js
+++ script/app.js
@@ -1,12 +1,65 @@
-document.addEventListener("DOMContentLoaded", () => {
-  var typed = new Typed('.animate', {
-    strings: [
-      'Frontend Developer',
-      'UI Designer',
-      'MERN Stack Developer',
-    ],
-    typeSpeed: 50,
-    backSpeed: 50,
-    loop: true,
+window.addEventListener("DOMContentLoaded", () => {
+  
+  const nav = document.querySelector(".nav"),
+  navList = document.querySelectorAll("li"),
+  totlaNavList = navList.length,
+  allSection = document.querySelectorAll(".section"),
+  totalSection = allSection.length;
+  for(let i=0; i<totlaNavList; i++ ){
+      const a = navList[i].querySelector("a");
+      a.addEventListener("click", function(){
+          for (let i = 0; i<totalSection; i++) {
+              allSection[i].classList.remove("active");
+          }
+          for(let j = 0; j<totlaNavList; j++){
+              if(navList[j].querySelector("a").classList.contains("active")){
+                  allSection[j].classList.add("back-section");
+              }
+              navList[j].querySelector("a").classList.remove("active");
+          }
+          a.classList.add("active");
+          showSection(this);
+          if(window.innerWidth < 1200){
+              asideSectionTogglerBtn();
+          }
+      })
+  }
+
+  function showSection(element){
+      for (let i = 0; i<totalSection; i++) {
+          allSection[i].classList.remove("active");
+      }
+      const target = element.getAttribute("href").split("#")[1];
+      document.querySelector("#" + target).classList.add("active");
+  }
+
+  function updateNav (element) {
+      for(let i = 0; i<totlaNavList; i++){
+          navList[i].querySelector("a").classList.remove("active");
+          const target = element.getAttribute("href").split("#")[1];
+          if(target === navList[i].querySelector("a").getAttribute("href").split("#")[i]){
+              navList[i].querySelector("a").classList.add("active");
+          }
+      }
+  }
+
+  document.querySelector(".hire-me").addEventListener("click", function () {
+      const sectionIndex = this.getAttribute("data-section-index")
+      showSection(this);
+      updateNav(this);
   })
+
+  const navTogglerBtn = document.querySelector(".nav-toggler"),
+      aside = document.querySelector(".aside");
+      navTogglerBtn.addEventListener("click", () => {
+          asideSectionTogglerBtn();
+      })
+      function asideSectionTogglerBtn() {
+          aside.classList.toggle("open");
+          navTogglerBtn.classList.toggle("open");
+          for(let i = 0; i<totalSection; i++){
+              allSection[i].classList.toggle("open")
+          }
+      }
+  
 });
